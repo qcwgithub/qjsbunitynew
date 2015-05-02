aeb7bbce9626e1f649165f348d1d1e35a62e6704
qcwgithub <doitmyway@qq.com>
2015-05-02 16:36:46 +0800

    perform test 1

---
+++ proj/Assets/JSBinding/Samples/PerformanceTest1.cs
@@ -0,0 +1,28 @@
+using SharpKit.JavaScript;
+using UnityEngine;
+using System.Collections;
+[JsType(JsMode.Clr,"../../StreamingAssets/JavaScript/SharpKitGenerated/JSBinding/Samples/PerformanceTest1.javascript")]
+public class PerformanceTest1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var sw = new System.Diagnostics.Stopwatch();
+        sw.Start();
+        var g = 0;
+        var f = 10;
+        for (var i = 0; i < 100000000; i++)
+        {
+            g += 1;
+            f += 10;
+        }
+        sw.Stop();
+        Debug.Log("calc result: " + (f + g).ToString());
+        Debug.Log("time: " + sw.ElapsedMilliseconds + " ms");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+	
+	}
+}
