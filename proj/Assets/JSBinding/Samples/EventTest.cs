497b027a70dd16b6e37544be201328fd582bea16
qcwgithub <doitmyway@qq.com>
2015-08-25 20:13:03 +0800

    +EventTest

---
+++ proj/Assets/JSBinding/Samples/EventTest.cs
@@ -0,0 +1,52 @@
+using SharpKit.JavaScript;
+using UnityEngine;
+using System.Collections;
+using System;
+
+//此类做CS and JS bindings
+[JsType(JsMode.Clr,"../../StreamingAssets/JavaScript/SharpKitGenerated/JSBinding/Samples/LoginManager.javascript")]
+public class LoginManager
+{
+    private static LoginManager _instance;
+    public static LoginManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new LoginManager();
+            }
+            return _instance;
+        }
+    }
+
+     public event Action onHaConnected;
+}
+
+
+//此类转JS
+[JsType(JsMode.Clr,"../../StreamingAssets/JavaScript/SharpKitGenerated/JSBinding/Samples/TestLogin.javascript")]
+public class TestLogin
+{
+    private void Dofun()
+    {
+	    LoginManager.Instance.onHaConnected += HandleonHaConnected;
+    }
+
+    private void HandleonHaConnected()
+    {
+    }
+}
+[JsType(JsMode.Clr,"../../StreamingAssets/JavaScript/SharpKitGenerated/JSBinding/Samples/EventTest.javascript")]
+public class EventTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
