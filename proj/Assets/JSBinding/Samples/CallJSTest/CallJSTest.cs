673738c0d6eef034cfbe778a9f3502b4fe24f21e
qiucw <doitmyway@qq.com>
2015-09-06 22:59:02 +0800

    +CallJSTest

---
+++ proj/Assets/JSBinding/Samples/CallJSTest/CallJSTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallJSTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        JSMgr.vCall.CallJSFunctionName(0, "CreateJSBindingInfo");
+        object obj = JSMgr.datax.getObject((int)JSApi.GetType.JSFunRet);
+        CSRepresentedObject csObj = (CSRepresentedObject)obj;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ proj/Assets/StreamingAssets/JavaScript/SharpKitGenerated/JSBinding/Samples/CallJSTest/CallJS.javascript
@@ -0,0 +1,10 @@
+var CreateJSBindingInfo = function () {
+    return {
+        Version: "1.5.0",
+        QQGroup: 189738580,
+        isForiOSUpdate: true,
+        getDocumentUrl: function () {
+            return "http://www.cnblogs.com/answerwinner/p/4469021.html";
+        }
+    };
+};
