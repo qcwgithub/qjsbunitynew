380408a2d3a5be0bbc77d84a4e0734cbf8d00e27
qiucw <doitmyway@qq.com>
2015-11-01 21:51:13 +0800

    + JSAttributes.cs

---
+++ proj/Assets/JSBinding/Source/JSAttributes.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/*
+ * 用于指定脚本要使用的 JSComponent 名
+ * 
+ * 例如
+ * JSComponent(Name = "JSComponent_EventTrigger")
+ */
+[AttributeUsage(AttributeTargets.Class)]
+public class JSComponentAttribute : Attribute
+{
+    public string Name { get; set; }
+}
